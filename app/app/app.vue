b5e7f0c2af6f08d3928fc762100ee7b0b5e1969b
jasonz0704 <oonetwome@gmail.com>
2026-04-29 14:01:45 -0400

    frontend design pt1

--- app/app/app.vue
+++ app/app/app.vue
@@ -1,6 +1,34 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="sidebar">
+    <nuxt-link to="/Home">
+      <div class="mainheader">Fake Instagram</div>
+    </nuxt-link>
+
+    <nuxt-link to="/Events">
+      <div class="navigation">To Events</div>
+    </nuxt-link>
+
+    <nuxt-link to="/Login">
+      <div class="navigation">Login</div>
+    </nuxt-link>
   </div>
+
+  <NuxtPage class="page" />
 </template>
+
+<script></script>
+
+<style scoped>
+.sidebar {
+  width: 300px;
+  background-color: bisque;
+}
+.mainheader {
+  font-size: 40px;
+  margin: 20px auto;
+}
+.navigation {
+  font-size: 25px;
+  margin: 15px;
+}
+</style>
---
+++ app/app/pages/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Login</div>
+
+  <div>Email:</div>
+  <input placeholder="Email Here" />
+
+  <div>Password:</div>
+  <input type="password" placeholder="Password" />
+</template>
+
+<script></script>
+
+<style scoped></style>
