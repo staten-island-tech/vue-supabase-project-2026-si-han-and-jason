9c75771e766e990d42c87bb87b9ed8de1d3ff2a3
Xacbular <jacobwu2592@gmail.com>
2026-05-04 13:46:07 -0400

    fohhh

--- app/app/app.vue
+++ app/app/app.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">{{ todo.name }}</li>
+  </ul>
 </template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import { createClient } from "@supabase/supabase-js";
+
+const config = useRuntimeConfig();
+const supabase = createClient(
+  config.public.supabaseUrl,
+  config.public.supabaseKey,
+);
+
+const todos = ref([
+  { id: 1, name: "Todo 1" },
+  { id: 2, name: "Todo 2" },
+  { id: 3, name: "Todo 3" },
+]);
+
+async function getTodos() {
+  const { data } = await supabase.from("todos").select();
+  todos.value = data;
+}
+
+onMounted(() => {
+  getTodos();
+});
+</script>
